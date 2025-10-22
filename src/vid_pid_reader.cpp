b3b9af69f5146e0f5b0a426a10241c48f432253f
KK <kk@retailnext.net>
2025-10-22 16:28:11 -0700

    AUR-1721 Update CMake files and vid_pid_reader

--- src/vid_pid_reader.cpp
+++ src/vid_pid_reader.cpp
@@ -1,40 +1,72 @@
 /*******************************************************************************
  *
  * Copyright (c) 2020 Albert Krenz
- * 
+ *
  * This code is licensed under BSD + Patent (see LICENSE.txt for full license text)
  *******************************************************************************/
-
  /* SPDX-License-Identifier: BSD-2-Clause-Patent */
+
 #include "vid_pid_reader.h"
-
+#include <libusb-1.0/libusb.h>
+#include <iostream>
+#include <stdexcept>
+#include <sstream>
 #include <vector>
-#include <fstream>
-#include <boost/algorithm/string.hpp>
-#include <boost/log/trivial.hpp>
+#include <cstring>
 
-std::tuple<int, int> VIDPIDReader::getVidPidForDev(std::string dev){
-  std::vector<std::string> results;
-  boost::split(results, dev, [](char c){return c == '/';});
-  
-  std::string pid_path = std::string("/sys/class/tty/") + results.back() + std::string("/../../../../idProduct");
-  BOOST_LOG_TRIVIAL(info) << "Read PID from " << pid_path;
-  std::ifstream pid_ifs(pid_path.c_str());
-  if(!pid_ifs.is_open()){
-    throw std::runtime_error("Could not read PID");
+std::tuple<int, int> VIDPIDReader::getVidPidForDev(std::string dev_path) {
+  libusb_context* ctx = nullptr;
+  libusb_device** devs = nullptr;
+  libusb_device_handle* handle = nullptr;
+
+  int r = libusb_init(&ctx);
+  if (r < 0) {
+    throw std::runtime_error("libusb_init failed");
   }
 
-  std::string vid_path = std::string("/sys/class/tty/") + results.back() + std::string("/../../../../idVendor");
-  BOOST_LOG_TRIVIAL(info) << "Read VID from " << vid_path;
-  std::ifstream vid_ifs(vid_path.c_str());
-  if(!vid_ifs.is_open()){
-    throw std::runtime_error(std::string("Could not read VID for ") + dev);
+  ssize_t cnt = libusb_get_device_list(ctx, &devs);
+  if (cnt < 0) {
+    libusb_exit(ctx);
+    throw std::runtime_error("libusb_get_device_list failed");
   }
-  int vid = 0;
-  int pid = 0;
-  pid_ifs >> std::hex >> pid;
-  vid_ifs >> std::hex >> vid;
 
-  BOOST_LOG_TRIVIAL(info) << "VID/PID for " << dev << " are 0x" << std::hex << vid << "/0x" << std::hex << pid;
-  return std::make_tuple(vid, pid);
+  int found_vid = -1;
+  int found_pid = -1;
+
+  for (ssize_t i = 0; i < cnt; i++) {
+    libusb_device* dev = devs[i];
+    libusb_device_descriptor desc;
+
+    if (libusb_get_device_descriptor(dev, &desc) != 0)
+      continue;
+
+    if (libusb_open(dev, &handle) == 0) {
+      unsigned char serial[256];
+      int len = 0;
+
+      if (desc.iSerialNumber > 0) {
+          len = libusb_get_string_descriptor_ascii(handle, desc.iSerialNumber, serial, sizeof(serial));
+      }
+
+      std::string serial_str = (len > 0) ? std::string((char*)serial, len) : "";
+
+      if (!serial_str.empty() && dev_path.find(serial_str) != std::string::npos) {
+        found_vid = desc.idVendor;
+        found_pid = desc.idProduct;
+        libusb_close(handle);
+        break;
+      }
+
+      libusb_close(handle);
+    }
+  }
+
+  libusb_free_device_list(devs, 1);
+  libusb_exit(ctx);
+
+  if (found_vid == -1 || found_pid == -1) {
+    throw std::runtime_error("Could not find matching USB device for " + dev_path);
+  }
+
+  return std::make_tuple(found_vid, found_pid);
 }
